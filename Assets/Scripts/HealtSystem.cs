28d5bb64f3422e269d5abca907b189d09d666266
Raghav-upes <ssrh7454@gmail.com>
2024-03-25 16:06:25 +0530

    Zombie Attack Done

--- Assets/Scripts/HealtSystem.cs
+++ Assets/Scripts/HealtSystem.cs
@@ -58,6 +58,7 @@ public class HealthSystem : MonoBehaviour
             }
 
         }
+      
     }
     private void SetupHealthBar(Canvas Canvas, Camera Camera)
     {
---
+++ Assets/Scripts/ZombieAttack.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ZombieAttack : MonoBehaviour
+{
+    private Animator anim;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        Debug.DrawRay(transform.position, transform.forward * 1f, Color.red);
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 1f))
+        {
+
+            if (hit.collider.gameObject.tag == "Heroes")
+            {
+                anim.SetBool("Attack", true);
+            }
+        }
+    }
+}
