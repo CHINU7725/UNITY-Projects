d01dc7244393172aa45de9f08f8b7d8abd23a958
Siddharth Verma <siddharthverma424@gmail.com>
2024-04-15 23:41:26 +0530

    Level Designing

--- Assets/Platformer_2/Scripts/SpawnManager.cs
+++ Assets/Platformer_2/Scripts/SpawnManager.cs
@@ -7,7 +7,7 @@ public class SpawnManager : MonoBehaviour
 
 
     [SerializeField] GameObject spawnPoint;
-    [SerializeField] List<GameObject> prefabsToSpawn;
+    [SerializeField] List<GameObject> LeveList;
     public Transform DroneSpawnPoint;
     public GameObject DroneSpawn;
 
@@ -17,9 +17,10 @@ public class SpawnManager : MonoBehaviour
         {
             Destroy(other.gameObject);
             Instantiate(DroneSpawn, DroneSpawnPoint.position, Quaternion.identity);
-            int index = Random.Range(0, prefabsToSpawn.Count - 1);
-            GameObject spawn = prefabsToSpawn[index];
-            Instantiate(spawn, spawnPoint.transform.position, Quaternion.identity);
+            int index = Random.Range(0, LeveList.Count - 1);
+            GameObject spawn = LeveList[index];
+            GameObject SpawnedLevel =  Instantiate(spawn, spawnPoint.transform.position, Quaternion.identity);
+            SpawnedLevel.GetComponent<LevelDesigning>().PlaceObjects();
         }
         else
         {
